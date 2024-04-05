7a09a63e8e375aafcda648b2f56da3609948e24c
我的袜子都是洞 <ruanfm@qq.com>
2024-04-05 16:19:39 +0800

    MySQL工具类增加单例模式

--- water_info/dao/mysqltool.py
+++ water_info/dao/mysqltool.py
@@ -1,7 +1,14 @@
 import pymysql
 from config.settings import DATABASE_CONFIG
 
-class MySQLTool(object):
+class Singleton(type):
+    _instances = {}
+    def __call__(cls, *args, **kwargs):
+        if cls not in cls._instances:
+            cls._instances[cls] = super(Singleton, cls).__call__(*args, **kwargs)
+        return cls._instances[cls]
+
+class MySQLTool(metaclass=Singleton):
     def __init__(self) -> None:
         c = DATABASE_CONFIG['dev']
         self.connection = pymysql.connect(

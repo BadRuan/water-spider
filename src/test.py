fe731113136fc8ba6c5a10220464a25a268176a6
ruanfumin <ruanfm@qq.com>
2024-04-21 13:00:46 +0800

    优化

---
+++ src/test.py
@@ -0,0 +1,5 @@
+from util.datetool import get_recently_time_range
+
+if __name__ == "__main__":
+    d = get_recently_time_range()
+    print(d)
--- src/util/datetool.py
+++ src/util/datetool.py
@@ -1,14 +1,20 @@
 from datetime import datetime, timedelta
+from pydantic import BaseModel
+from typing import List
 from config.settings import DATE_RANGE_LENGTH
 
-date_range = {"btime": "", "etime": ""}
 formatStr = "%Y%m%d%H%M"  # 时间示例: 202401041200
 
 
-# 以输入时间为结束时间，生成指定范围长度的时间范围
+class DateRange(BaseModel):
+    btime: str
+    etime: str
+
+
+# 输入结束时间，生成指定范围长度的时间范围
 def get_time_range(
-    input_datetime_str: str, days: int = DATE_RANGE_LENGTH["normal"]
-) -> str:
+    input_datetime_str: str, day_length: int = DATE_RANGE_LENGTH["normal"]
+) -> DateRange:
     # 日期格式: 202405011200
     if len(input_datetime_str) != 12:
         raise ValueError("时间长度不对, 参考这个: 202405010000")
@@ -16,22 +22,20 @@ def get_time_range(
     if input_datetime > datetime.now():
         input_datetime = datetime.now()
     # 确定开始时间
-    days_ago = input_datetime - timedelta(days=days)
-    date_range = {
-        "btime": days_ago.strftime(formatStr),
-        "etime": input_datetime.strftime(formatStr),
-    }
-    return date_range
+    days_ago = input_datetime - timedelta(days=day_length)
+    return DateRange(
+        btime=days_ago.strftime(formatStr), etime=input_datetime.strftime(formatStr)
+    )
 
 
 # 获取最近时间范围
-def get_recently_time_range():
+def get_recently_time_range() -> DateRange:
     now_time: str = datetime.now().strftime(formatStr)
     return get_time_range(now_time)
 
 
 # 获取初始化日期列表
-def get_init_date_list(day: int = DATE_RANGE_LENGTH["init"]) -> list:
+def get_init_date_list(day: int = DATE_RANGE_LENGTH["init"]) -> List[DateRange]:
     # 获取当年的第一天
     this_year = datetime.now().year
     start_date = datetime(this_year, 1, 1)
@@ -52,8 +56,5 @@ def get_init_date_list(day: int = DATE_RANGE_LENGTH["init"]) -> list:
 
 
 # 获取初始化日期范围列表
-def get_init_data_range_list(day: int = DATE_RANGE_LENGTH["init"]) -> list:
-    date_range_list = []
-    for init_day in get_init_date_list(day):
-        date_range_list.append(get_time_range(init_day, day))
-    return date_range_list
+def get_init_data_range_list(day: int = DATE_RANGE_LENGTH["init"]) -> List[DateRange]:
+    return [get_time_range(init_day, day) for init_day in get_init_date_list(day)]

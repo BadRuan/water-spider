24c82c440e6a551794d19d598c2337b89d8488b4
我的袜子都是洞 <ruanfm@qq.com>
2026-04-07 22:25:58 +0800

    初步完成项目

--- src/engine.py
+++ src/engine.py
@@ -1,42 +1,61 @@
 from typing import List
 from datetime import datetime
 from time import sleep
-from src.settings import STATIONS, DATE_SETTINGS
+from src.settings import STATIONS 
 from src.utils.logger import Logger
 from src.utils.datetool import get_time_range, formatStr
-from src.model import RequestDateRange, Request
-from src.handle import Handler, SendApiHandler, DecodeHandler
+from src.utils.storage import recoder_count_change
+from src.model import DateRange, Request
+from src.handle import Handler, SendApiHandler, DecodeHandler, StorageHandle
 
 
-logger = Logger(__name__)
+log = Logger(__name__)
 
 class Spider:
-    
-    def __init__(self) -> None:
-        logger.info(DATE_SETTINGS)
 
-    def _get_data(self, target_date: List[RequestDateRange]):
+    @recoder_count_change
+    def _get_data(self, target_date: List[DateRange]):
         for date_time in target_date:
+            log.info("---------------------")
+            log.info(f"开始获取时间 {date_time.start_time} => {date_time.end_time} 的数据")
+            count: int = 0
             for station in STATIONS:
-                request: Request = Request(station=station,date_range=date_time)
+                request: Request = Request(code=station.code, name=station.name,date_range=date_time)
                 api_handle: Handler = SendApiHandler() # 发送请求
                 decode_handle: Handler = DecodeHandler() # 解析数据
+                storage_handle: Handler = StorageHandle() # 存储数据
                 api_handle.set_next(decode_handle)
+                decode_handle.set_next(storage_handle)
+                
                 api_handle.handle(request)
-                sleep(1) # 休眠1秒
-                logger.info(request)
-
+                _count: int = len(request.data)
+                count += _count
+                log.info(f"成功获取{request.name} {_count} 条数据")
+                sleep(0.5)
+            log.info(f"本轮共同获取 {count} 条水位数据")
 
     def run_in_24_hour(self):
-        # while True:
-        target_date: List[RequestDateRange] = get_time_range(end_datetime_str=None,start_datetime_str=None)
-        self._get_data(target_date)
-            
+        while True:
+            target_date: List[DateRange] = get_time_range(end_datetime_str=None,start_datetime_str=None)
+            self._get_data(target_date)
+            log.info("休息 5 秒继续")
+            sleep(5)
 
-    def run_get_this_year_full_data(self):
+    def get_this_year_full_data(self):
         now: datetime = datetime.now()
         this_year_start: datetime = datetime(now.year, 1, 1)
-        target_date: List[RequestDateRange] = get_time_range(end_datetime_str=now.strftime(formatStr),start_datetime_str=this_year_start.strftime(formatStr))
+        target_date: List[DateRange] = get_time_range(end_datetime_str=now.strftime(formatStr),start_datetime_str=this_year_start.strftime(formatStr))
         self._get_data(target_date)
-            
+    
+    def get_target_year_full_data(self, year: int):
+        year_start: datetime = datetime(year, 1, 1)
+        year_end: datetime = datetime(year, 12, 31)
+        target_date: List[DateRange] = get_time_range(end_datetime_str=year_end.strftime(formatStr),start_datetime_str=year_start.strftime(formatStr))
+        self._get_data(target_date)
+
+    def get_target_date_range_data(self, start_time: str, end_time: str):
+        target_date: List[DateRange] = get_time_range(start_time, end_time)
+        self._get_data(target_date)
+
+           
 spider = Spider()
--- src/settings.py
+++ src/settings.py
@@ -1,4 +1,4 @@
-from typing import NamedTuple
+from typing import NamedTuple, List, Tuple
 from src.model import DateSetting, Station
 
 
@@ -11,14 +11,16 @@ class DataConfig(NamedTuple):
     
 postgres = DataConfig(url='100.95.218.64', user='postgres', password='E,*f*YdGgYSgqfze1tLqc0Pm8CK2', port=44455, database='water') 
 
-
 DATE_SETTINGS = DateSetting(latest_date_length=2, cut_date_length=20)
 
-STATIONS = [
-    Station(code=60115400,name="芜湖",water_items=[]),
-    # Station(code=62904400,name="凤凰颈闸下"),
-    Station(code=62900700,name="裕溪闸下",water_items=[]),
-    Station(code=62900600,name="裕溪闸上",water_items=[]),
-    Station(code=62906500,name="清水",water_items=[]),
-    Station(code=62905100,name="新桥闸上",water_items=[])
+station_list: List[Tuple[int, str]] = [
+    (60115400,"芜湖"),
+    (62904400,"凤凰颈新站闸上"),
+    (62904500,"凤凰颈新站闸下"),
+    (62900700,"裕溪闸下"),
+    (62900600,"裕溪闸上"),
+    (62906500,"清水"),
+    (62905100,"新桥闸上")
 ]
+
+STATIONS = [Station(code=station[0], name=station[1], water_items=[]) for station in station_list]
